8ea986c95b86166e7e6992f36a0b8f91484f0d09
Tuan <phuongtuan8797@gmail.com>
2021-05-27 09:01:54 +0700

    Completed calculator design

---
+++ Calculator/ModRoundedButton.swift
@@ -0,0 +1,31 @@
+//
+//  ModRoundedButton.swift
+//  Calculator
+//
+//  Created by Mac on 26/05/2021.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable class RoundedButton: UIButton{
+    override func layoutSubviews() {
+        super.layoutSubviews();
+        updateCornerRadius();
+    }
+    @IBInspectable var rounded: Bool = false{
+        didSet{
+            updateCornerRadius()
+        }
+    }
+    func updateCornerRadius() {
+        if frame.size.height < frame.size.width {
+            layer.cornerRadius = rounded ? frame.size.height / 2 : 0;
+        }else{
+            layer.cornerRadius = rounded ? frame.size.width / 2 : 0;
+        }
+    }
+    
+    
+    
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,10 +14,60 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
     override var preferredStatusBarStyle: UIStatusBarStyle{
         return .lightContent;
     }
+    
+    
+    
+    func ChangeColorWhenPress(Target: UIButton){
+        if (Target.backgroundColor?.cgColor == UIColor.systemGray.cgColor) {
+            Target.backgroundColor = UIColor.lightGray;
+        }
+        if (Target.backgroundColor?.cgColor == UIColor.systemOrange.cgColor){
+            Target.backgroundColor = UIColor.lightBrownMod;
+        }
+        if (Target.backgroundColor?.cgColor == UIColor.secondaryLabel.cgColor) {
+            Target.backgroundColor = UIColor.lightSecondaryMod;
+        }
+    }
+    
+    func ReturnToOriginalColor(Target: UIButton) {
+        if (Target.backgroundColor?.cgColor == UIColor.lightGray.cgColor) {
+            Target.backgroundColor = UIColor.systemGray;
+        }
+        if (Target.backgroundColor?.cgColor == UIColor.lightBrownMod.cgColor) {
+            Target.backgroundColor = UIColor.systemOrange;
+        }
+        if (Target.backgroundColor?.cgColor == UIColor.lightSecondaryMod.cgColor) {
+            Target.backgroundColor = UIColor.secondaryLabel;
+        }
+    }
 
-
+    @IBAction func HoldRelease(_ sender: UIButton) {
+        ReturnToOriginalColor(Target: sender)
+    }
+    
+    @IBAction func HoldDown(_ sender: UIButton) {
+        ChangeColorWhenPress(Target: sender)
+    }
+    
 }
+
+extension UIColor {
+    class var lightBrownMod: UIColor{
+        return UIColor(red: 204.0/255.0, green: 153.0/255.0, blue: 102.0/255.0, alpha: 1.0);
+    }
+    class var lightSecondaryMod: UIColor{
+        return UIColor(red: 102.0/255.0, green: 102.0/255.0, blue: 102.0/255.0, alpha: 1.0);
+    }
+    
+}
+
+
+
 
